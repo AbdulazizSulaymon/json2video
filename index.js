b565b4a0b11c2c4468c65eaa2b33c2583d8018d5
AbdulazizSulaymon <djshahrick@gmail.com>
2022-01-08 15:41:27 +0500

    123

---
+++ index.js
@@ -0,0 +1,37 @@
+const instance = axios.create({
+  baseURL: "https://api.json2video.com/v1/",
+  timeout: 1000,
+  headers: {
+    "Content-Type": "application/json",
+    "x-api-key": "4a2PAVXDBD7cJpDzxmh8a4v8K7g9ox8F2HPpIxj2",
+  },
+});
+
+const createNewMovie = async () => {
+  const res = await instance.post("/movies", {
+    project: "tutorial",
+    resolution: "full-hd",
+    quality: "high",
+    scenes: [
+      {
+        "background-color": "#4392F1",
+        elements: [
+          {
+            type: "text",
+            items: [
+              {
+                text: "Hello world",
+              },
+            ],
+            y: 510,
+            duration: 10,
+          },
+        ],
+      },
+    ],
+  });
+
+  console.log(res);
+};
+
+createNewMovie();
